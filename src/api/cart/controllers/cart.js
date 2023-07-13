7ec93b53e9222ab30364bb7ccf05cedb6cc74b26
Claude De-Tchambila <christ.tchambila@gmail.com>
2023-07-13 21:18:54 +0200

    Add cart feature changes

--- src/api/cart/controllers/cart.js
+++ src/api/cart/controllers/cart.js
@@ -6,4 +6,14 @@
 
 const { createCoreController } = require('@strapi/strapi').factories;
 
-module.exports = createCoreController('api::cart.cart');
+module.exports = createCoreController('api::cart.cart', ({ strapi }) =>  ({
+  async create(ctx) {
+    // some logic here
+    console.log('cart controller', ctx);
+    console.log('body', ctx.request.body);
+    const response = await super.create(ctx);
+    // some more logic
+
+    return response;
+  }
+}));
