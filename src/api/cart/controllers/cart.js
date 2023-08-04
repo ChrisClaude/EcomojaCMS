4b867acf4da8d6a7e87cfa5a86433d472a6f65c3
ColeColombia <benzemabenelzane@yahoo.com>
2023-08-04 09:35:47 +0200

    finalize adding user by Id

--- src/api/cart/controllers/cart.js
+++ src/api/cart/controllers/cart.js
@@ -8,23 +8,27 @@ const { createCoreController } = require('@strapi/strapi').factories;
 module.exports = createCoreController('api::cart.cart', ({ strapi }) =>  ({
   async findOne(ctx){
     try{
-      console.log(ctx.params.id)
-      const userId = ctx.params.id
-      const cart = await strapi.db.query('api::cart.cart').findOne({ 
-        select: ['id'],
+      const userId = ctx.params.id;
+      console.log(userId);
+      const cart = await strapi.db.query('api::cart.cart').findMany({ 
+        select: ['*'],
         where: {
           users_permissions_user: userId},
           populate: {users_permissions_user:true, product:true}
         });
 
-      if(cart){
-        delete cart.users_permissions_user.provider
-        delete cart.users_permissions_user.password
-        delete cart.users_permissions_user.resetPasswordToken
-        delete cart.users_permissions_user.confirmationToken
-        delete cart.users_permissions_user.confirmed
-        delete cart.users_permissions_user.blocked
-        return cart
+        const newCart =  cart.map(item => {
+          delete item.users_permissions_user.provider;
+          delete item.users_permissions_user.password;
+          delete item.users_permissions_user.resetPasswordToken;
+          delete item.users_permissions_user.confirmationToken;
+          delete item.users_permissions_user.confirmed;
+          delete item.users_permissions_user.blocked;
+          return item;
+        })
+
+      if(newCart){
+        return newCart;
       }
 
       else {
