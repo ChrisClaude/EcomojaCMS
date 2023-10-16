25b957bf2e81f72764008ee71ca0dd06e3891e66
Benelzane <kholani.crazeenvironmental@gmail.com>
2023-10-16 10:46:54 +0200

    save local storage items

--- src/api/cart/controllers/cart.js
+++ src/api/cart/controllers/cart.js
@@ -9,7 +9,6 @@ module.exports = createCoreController('api::cart.cart', ({ strapi }) =>  ({
   async findByUserId(ctx){
     try{
       const userId = ctx.params.id;
-      console.log(userId);
       const cart = await strapi.db.query('api::cart.cart').findMany({ 
         select: ['*'],
         where: {
@@ -38,5 +37,45 @@ module.exports = createCoreController('api::cart.cart', ({ strapi }) =>  ({
     catch(err){
       ctx.body = err
     }
-  }
-}));
+  },
+  async saveLocalStorageCart(ctx){
+    try{
+      const cart = ctx.request.body;
+      const userId = cart[0].Users_permissions_user.id;
+      const entries = await strapi.db.query('api::cart.cart').findMany({
+        select: ['*'],
+        where: { users_permissions_user: userId},
+        populate: { product: true, quantity:true },
+      });
+
+      if(cart){
+        if(entries){
+          cart.forEach(async (item) => {
+            const cartItem = entries.find((cart)=> cart.product.id === item.product.id);
+            if(cartItem){
+              const qty = parseInt(item.productInstances) + parseInt(cartItem.quantity);
+              const entry = await strapi.db.query('api::cart.cart').update({
+                where: { users_permissions_user: userId, product: cartItem.product.id },
+                data: {
+                  quantity: qty,
+                },
+              });
+            }
+            else{
+              const entry = await strapi.db.query('api::cart.cart').create({
+                data: {
+                  users_permissions_user: userId,
+                  product: item.product.id,
+                  quantity: item.productInstances,
+                },
+              });
+            }
+          });
+        }
+        
+      }
+    }
+    catch(error){
+      console.log(error);
+    }
+},}));
